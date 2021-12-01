9e44a22dccdfa4ce7b20104d79f9c37ddc444927
mary <47014666+magicmarymoore@users.noreply.github.com>
2021-11-30 22:55:38 -0700

    added comments

--- day1/day1.py
+++ day1/day1.py
@@ -1,27 +1,34 @@
-import day1Data
+import day1Data # file w/given data for the problem
 
-data = day1Data.data.split('\n')
+# creates a list of all the data points
+data = day1Data.data.split('\n') # each number is on it's own line, so they're separated by newline char (\n)
 
 for i, x in enumerate(data):
-   data[i] = int(x)
+   data[i] = int(x) # converts each value in the list into a number
 
+
+# part 1
 def partOne(inData):
-   increases = 0
+   increases = 0 # number of increases inbetween numbers
+
    for i, x in enumerate(inData):
-      if (i == 0):
+      if (i == 0): # skip the first number b/c there isn't one before it to compare it to
           continue
-      if x > data[i-1]:
+      if x > data[i-1]: # if the current value is larger than the one before it, increase total
          increases += 1
    
-   return increases
+   return increases # return total number of pairs where there was an increase
 
 print(partOne(data))
 
+
+# part 2
 def partTwo(inData):
-   increases = 0
+   increases = 0 # nunber of increases inbetween sums of the sets of three numbers
 
    for i, x in enumerate(inData):
-      if (i+3 < len(inData)):
+      if (i+3 < len(inData)): # make sure to end 3 before the end so no issues w/data that doesn't exist
+         # only need to compare current value and the value 3 indexes more --> other two values will be the same for the sums lol
          if (inData[i+3] > x): increases += 1
 
    return increases
